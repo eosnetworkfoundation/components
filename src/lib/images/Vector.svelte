87bd030a6e9ebff1fe8be6219faf1575673a44b1
Alex  Griffith <alexgriffith@Alexs-MBP.home>
2023-06-20 14:33:17 -0400

    add tests for primary button

--- src/lib/images/Vector.svelte
+++ src/lib/images/Vector.svelte
@@ -1,4 +1,5 @@
 <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <title>checkmark</title>
     <path
         fill-rule="evenodd"
         clip-rule="evenodd"
