7dd5599884a3d9a3e73fa3e5d479fd49e9343976
Kelly Tran <kellytran3k@gmail.com>
2023-06-21 03:22:42 -0400

    deleted unneeded styling

--- src/components/TextField.svelte
+++ src/components/TextField.svelte
@@ -12,12 +12,12 @@
     let isFocused = false;
     let isValid = true;
 
-    const emptyState =
-        'w-full h-68px font-suisse text-18px text-left bg-transparent border border-gray-600 text-white focus:outline-none transition duration-500 ease-in-out rounded-lg';
+    const emptyState = 
+        'w-full h-68px font-suisse text-[18px] text-left bg-transparent border border-gray-600 text-white focus:outline-none transition duration-500 ease-in-out rounded-lg';
     const focusedState =
-        'w-full h-68px font-suisse text-18px text-left bg-transparent border border-purple-500 text-white focus:outline-none transition duration-500 ease-in-out rounded-lg';
+        'w-full h-68px font-suisse text-[18px] text-left bg-transparent border border-purple-500 text-white focus:outline-none transition duration-500 ease-in-out rounded-lg';
     const filledState =
-        'w-full h-68px font-suisse text-18px text-left bg-transparent border border-red-500 text-white focus:outline-none transition duration-500 ease-in-out rounded-lg';
+        'w-full h-68px font-suisse text-[18px] text-left bg-transparent border border-red-500 text-white focus:outline-none transition duration-500 ease-in-out rounded-lg';
 
     onMount(() => {
         if (value) isFocused = true;
@@ -48,16 +48,16 @@
     $: handleState = () => {
         if (value === '') {
             return emptyState;
-        }
+        } 
 
         if (isFocused) {
             return focusedState;
         }
         return isValid ? emptyState : filledState;
-    };
-</script>
+    }
 
-<div class="relative h-68px w-602px absolute top-52 left-5 p-4 rounded-lg flex items-center">
+</script>
+<div class="relative w-[600px] absolute p-4 rounded-lg flex items-center">
     <input
         bind:value
         type="text"
@@ -75,7 +75,7 @@
         {:else}
             <button
                 on:click={handlePaste}
-                class="text-purple-600 font-semibold text-18px"
+                class="text-purple-600 font-semibold text-[18px]"
                 style="font-family: 'Open Sans', sans-serif; height: 25px; width: 48px;"
             >
                 Paste
@@ -85,7 +85,7 @@
     {#if showWarning}
         <p
             transition:fade={{ duration: 500 }}
-            class="text-red-500 font-suisse text-18px leading-6 absolute bottom-0 transform translate-y-1/2"
+            class="text-red-500 font-suisse text-[18px] leading-6 absolute bottom-0 transform translate-y-1/2"
         >
             {errorText}
         </p>
@@ -93,15 +93,6 @@
 </div>
 
 <style>
-    .w-602px {
-        width: 602px;
-    }
-    .text-18px {
-        font-size: 18px;
-    }
-    .w-full {
-        width: 100%;
-    }
     * {
         box-sizing: border-box;
     }
